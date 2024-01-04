ecb55a5829dda981cfcc343fc074bb5562a1101a
vincent <vincent@vincentdeMacBook-Pro.local>
2024-01-05 01:44:57 +0800

    feat: 两个链表求和

---
+++ algorithms/addTwoNumbers.js
@@ -0,0 +1,79 @@
+// 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
+// 请你将两个数相加，并以相同形式返回一个表示和的链表。
+// 你可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+
+class ListNode{
+  constructor(value, next) {
+   this.val = value;
+   this.next = next;
+  }
+}
+var addTwoNumbers = function(l1, l2) {
+  // 定义头节点和尾节点为 null，用于构建结果链表
+  let head = null, tail = null;
+  // 初始化进位为 0
+  let carry = 0;
+  // 当 l1 或 l2 都不为 null 时，执行循环
+  while (l1 || l2) {
+      // 获取当前 l1 和 l2 节点的值，若为空则默认为 0
+      const n1 = l1 ? l1.val : 0;
+      const n2 = l2 ? l2.val : 0;
+      // 计算当前位的和，包括进位
+      const sum = n1 + n2 + carry;
+      // 判断结果链表是否为空，若为空则创建新的头节点和尾节点
+      if (!head) {
+          head = tail = new ListNode(sum % 10);
+      } else {
+          // 若不为空，则将新节点连接到尾节点后面，并更新尾节点为新的节点
+          tail.next = new ListNode(sum % 10);
+          tail = tail.next;
+      }
+      // 更新进位
+      carry = Math.floor(sum / 10);
+      // 移动到下一个节点，如果节点存在的话
+      if (l1) {
+          l1 = l1.next;
+      }
+      if (l2) {
+          l2 = l2.next;
+      }
+  }
+  // 循环结束后，检查最后一位是否有进位，有的话在结果链表末尾添加新节点
+  if (carry > 0) {
+      tail.next = new ListNode(carry);
+  }
+  // 返回结果链表的头节点
+  return head;
+};
+
+
+// 作者：力扣官方题解
+// 链接：https://leetcode.cn/problems/add-two-numbers/solutions/435246/liang-shu-xiang-jia-by-leetcode-solution/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+const head13 = new ListNode(3, )
+const head12 = new ListNode(4, head13)
+const l1 = new ListNode(2, head12)
+
+
+const head23 = new ListNode(4, )
+const head22 = new ListNode(6, head23)
+const l2 = new ListNode(5, head22)
+
+const result = addTwoNumbers(l1, l2)
+console.log(result)
---
+++ algorithms/invertTree.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var invertTree = function(root) {
+  if (root === null) {
+      return null;
+  }
+  const left = invertTree(root.left);
+  const right = invertTree(root.right);
+  root.right = left;
+  root.left = right;
+  return root;
+};
+
+
+
+const root = {
+  val: 3,
+  left: { val: 9, left: null, right: null },
+  right: {
+    val: 20,
+    left: { val: 15, left: null, right: null },
+    right: { val: 7, left: null, right: null },
+  },
+};
+
+const r = invertTree(root);
+console.log(r, r.lef, r.right);
